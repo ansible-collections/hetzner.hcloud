40c15e2c9f115f84a0c3c56ed8e8b41d994534bc
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2020-04-15 14:51:45 +0200

    debug

--- tests/utils/gitlab/hcloud.sh
+++ tests/utils/gitlab/hcloud.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-cloud="hcloud"
-python="3.8"
-
-target="cloud/hcloud/"
-HCLOUD_TOKEN= $(cat hcloud_token.txt)
+HCLOUD_TOKEN=$(cat hcloud_token.txt)
 changed_all_target="shippable/${cloud}/smoketest/"
 
 echo "[default]
@@ -12,6 +8,6 @@ hcloud_api_token=${HCLOUD_TOKEN}
 " >> $(pwd)/tests/integration/cloud-config-hcloud.ini
 # shellcheck disable=SC2086
 export SHIPPABLE="true"
-export SHIPPABLE_BUILD_NUMBER="gl-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)"
+export SHIPPABLE_BUILD_NUMBER="gl-$(cat prefix.txt)"
 export SHIPPABLE_JOB_NUMBER="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 2 | head -n 1)"
-ansible-test integration --color --local -v "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"}
+ansible-test integration --color --local -v "cloud/hcloud/" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"}
