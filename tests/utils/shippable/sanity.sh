7f3d890223db008e14ae3fd30668bc077e6d4140
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2020-07-31 07:32:45 +0200

    Fix tests
    Signed-off-by: Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>

--- tests/utils/shippable/sanity.sh
+++ tests/utils/shippable/sanity.sh
@@ -14,7 +14,9 @@ else
 fi
 
 if [ "${group}" == "extra" ]; then
-    /usr/bin/python3.7 -m pip3 install antsibull-changelog
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    /usr/bin/python3.7 get-pip.py --user
+    /usr/bin/python3.7 -m pip install antsibull-changelog
     /usr/bin/python3.7 ../../community/internal_test_tools/tools/run.py --color
     exit
 fi
