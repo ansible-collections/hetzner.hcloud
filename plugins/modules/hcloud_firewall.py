0462432023d47d53e43ae2c5c75b8354d00a5e27
Rene Moser <mail@renemoser.net>
2021-04-06 10:17:55 +0200

    trigger tests

--- plugins/modules/hcloud_firewall.py
+++ plugins/modules/hcloud_firewall.py
@@ -3,6 +3,7 @@
 
 # Copyright: (c) 2020, Hetzner Cloud GmbH <info@hetzner-cloud.de>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
 
 from __future__ import absolute_import, division, print_function
 
