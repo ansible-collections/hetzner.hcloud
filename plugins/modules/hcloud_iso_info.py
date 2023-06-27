928e3854704963c492f6f1fd0ea8beb8e5ea68ff
jo <ljonas@riseup.net>
2023-06-27 12:08:40 +0200

    fixes and improvements

--- plugins/modules/hcloud_iso_info.py
+++ plugins/modules/hcloud_iso_info.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
 # Copyright: (c) 2022, Patrice Le Guyader
 # heavily inspired by the work of @LKaemmerling
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import absolute_import, division, print_function
 
-__metaclass__ = type
-
-DOCUMENTATION = '''
+DOCUMENTATION = """
 ---
 module: hcloud_iso_info
 
 short_description: Gather infos about the Hetzner Cloud ISO list.
-
 
 description:
     - Gather infos about the Hetzner Cloud ISO list.
@@ -36,16 +31,16 @@ options:
         description:
             - Filter ISOs with compatible architecture.
         type: str
-        choices: [ x86, arm ]
+        choices: [x86, arm]
     include_architecture_wildcard:
         description:
-            - Include ISOs with wildcard architecture (architecture is null). Works only if architecture filter is specified.
+            - Include ISOs with wildcard architecture (architecture is null).
+            - Works only if architecture filter is specified.
         type: bool
 
 extends_documentation_fragment:
-- hetzner.hcloud.hcloud
-
-'''
+    - hetzner.hcloud.hcloud
+"""
 
 EXAMPLES = """
 - name: Gather hcloud ISO type infos
@@ -79,30 +74,31 @@ hcloud_iso_info:
             type: str
             sample: Debian 12.0 (amd64/netinstall)
         architecture:
-            description: Type of cpu architecture this ISO is compatible with. None indicates no restriction on the architecture (wildcard).
-            returned: always
-            type: str, None
-            sample: x86
-        type:
-            description: Type of the ISO. Choices: `public`, `private`
+            description: >
+                Type of cpu architecture this ISO is compatible with.
+                None indicates no restriction on the architecture (wildcard).
             returned: always
             type: str
+            sample: x86
+        type:
+            description: Type of the ISO.
+            returned: always
+            type: str
+            choices: [public, private]
             sample: public
         deprecated:
-            description: ISO 8601 timestamp of deprecation, None if ISO is still available. After the deprecation time it will no longer be possible to attach the ISO to servers.
+            description: >
+                ISO 8601 timestamp of deprecation, None if ISO is still available.
+                After the deprecation time it will no longer be possible to attach the
+                ISO to servers.
             returned: always
-            type: datetime, None
+            type: str
             sample: "2024-12-01T00:00:00+00:00"
 """
 
-from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils._text import to_native
+from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.hetzner.hcloud.plugins.module_utils.hcloud import Hcloud
-
-try:
-    from hcloud import APIException
-except ImportError:
-    APIException = None
 
 
 class AnsibleHcloudIsoInfo(Hcloud):
@@ -114,27 +110,28 @@ class AnsibleHcloudIsoInfo(Hcloud):
         tmp = []
 
         for iso_info in self.hcloud_iso_info:
-            if iso_info is not None:
-                tmp.append({
+            if iso_info is None:
+                continue
+
+            tmp.append(
+                {
                     "id": to_native(iso_info.id),
                     "name": to_native(iso_info.name),
                     "description": to_native(iso_info.description),
                     "type": iso_info.type,
                     "architecture": iso_info.architecture,
-                    "deprecated": iso_info.deprecated
-                })
+                    "deprecated": iso_info.deprecated,
+                }
+            )
+
         return tmp
 
     def get_iso_infos(self):
         try:
             if self.module.params.get("id") is not None:
-                self.hcloud_iso_info = [self.client.isos.get_by_id(
-                    self.module.params.get("id")
-                )]
+                self.hcloud_iso_info = [self.client.isos.get_by_id(self.module.params.get("id"))]
             elif self.module.params.get("name") is not None:
-                self.hcloud_iso_info = [self.client.isos.get_by_name(
-                    self.module.params.get("name")
-                )]
+                self.hcloud_iso_info = [self.client.isos.get_by_name(self.module.params.get("name"))]
             else:
                 self.hcloud_iso_info = self.client.isos.get_all(
                     architecture=self.module.params.get("architecture"),
@@ -150,7 +147,7 @@ class AnsibleHcloudIsoInfo(Hcloud):
             argument_spec=dict(
                 id={"type": "int"},
                 name={"type": "str"},
-                architecture={"type": "str","choices": ["x86", "arm"]},
+                architecture={"type": "str", "choices": ["x86", "arm"]},
                 include_architecture_wildcard={"type": "bool"},
                 **Hcloud.base_module_arguments(),
             ),
@@ -163,10 +160,9 @@ def main():
     hcloud = AnsibleHcloudIsoInfo(module)
     hcloud.get_iso_infos()
     result = hcloud.get_result()
-    ansible_info = {
-        'hcloud_iso_info': result['hcloud_iso_info']
-    }
+    ansible_info = {"hcloud_iso_info": result["hcloud_iso_info"]}
     module.exit_json(**ansible_info)
+
 
 if __name__ == "__main__":
     main()
