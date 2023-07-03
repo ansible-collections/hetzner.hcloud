5d327db35ad48dfa1eb217f7d2bb50f1f6eba3e2
jo <ljonas@riseup.net>
2023-07-03 16:08:19 +0200

    make vendor script self contained

--- Makefile
+++ Makefile
@@ -1,18 +1,8 @@
 SHELL := bash
 .PHONY: vendor clean
 
-HCLOUD_URL = https://github.com/hetznercloud/hcloud-python
-HCLOUD_VERSION = main
-HCLOUD_VENDOR = plugins/module_utils/vendor/hcloud
-
 vendor:
-	git clone --depth=1 --branch=$(HCLOUD_VERSION) $(HCLOUD_URL) hcloud-python
-
-	rm -Rf $(HCLOUD_VENDOR)
-	mv hcloud-python/hcloud $(HCLOUD_VENDOR)
-	python3 scripts/vendor.py $(HCLOUD_VENDOR)
-
-	rm -Rf hcloud-python
+	python3 scripts/vendor.py
 
 clean:
 	git clean -xdf \
--- scripts/vendor.py
+++ scripts/vendor.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
 
+"""
+Fetch and bundles the hcloud package inside the collection.
+
+Fetch the desired version `HCLOUD_VERSION` from https://github.com/hetznercloud/hcloud-python
+`HCLOUD_SOURCE_URL` using git, apply some code modifications to comply with ansible,
+move the modified files at the vendor location `HCLOUD_VENDOR_PATH`.
+"""
+
+import logging
 import re
-from argparse import ArgumentParser
 from pathlib import Path
+from shutil import move, rmtree
+from subprocess import check_call
+from tempfile import TemporaryDirectory
 from textwrap import dedent
 
+logger = logging.getLogger("vendor")
 
-def main(package: Path) -> int:
-    for file in package.rglob("*.py"):
+HCLOUD_SOURCE_URL = "https://github.com/hetznercloud/hcloud-python"
+HCLOUD_VERSION = "main"
+HCLOUD_VENDOR_PATH = "plugins/module_utils/vendor/hcloud"
+
+
+def apply_code_modifications(source_path: Path):
+    for file in source_path.rglob("*.py"):
         content = file.read_text()
+        content_orig = content
 
         # Wrap requests imports
         content = re.sub(
@@ -45,14 +63,30 @@ def main(package: Path) -> int:
             content,
         )
 
+        if content != content_orig:
+            logger.info("Applied code modifications on %s", file)
+
         file.write_text(content)
+
+
+def main() -> int:
+    with TemporaryDirectory() as tmp_dir:
+        tmp_dir_path = Path(tmp_dir)
+        logger.info("Created temporary directory %s", tmp_dir_path)
+
+        check_call(["git", "clone", "--depth=1", "--branch", HCLOUD_VERSION, HCLOUD_SOURCE_URL, tmp_dir_path])
+        logger.info("Cloned the source files in %s", tmp_dir_path)
+
+        apply_code_modifications(tmp_dir_path / "hcloud")
+        logger.info("Applied code modifications on the source files")
+
+        rmtree(HCLOUD_VENDOR_PATH)
+        move(tmp_dir_path / "hcloud", HCLOUD_VENDOR_PATH)
+        logger.info("Bundled the modified sources files in the collection")
 
     return 0
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("package", help="path to the vendored package.")
-    args = parser.parse_args()
-
-    raise SystemExit(main(package=Path(args.package)))
+    logging.basicConfig(level=logging.INFO, format="%(levelname)-8s: %(message)s")
+    raise SystemExit(main())
