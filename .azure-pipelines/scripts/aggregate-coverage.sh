f46a1b297076443c50ebca16036359789af58548
jo <ljonas@riseup.net>
2023-07-26 16:45:25 +0200

    chore: fix shell script linting errors

--- .azure-pipelines/scripts/aggregate-coverage.sh
+++ .azure-pipelines/scripts/aggregate-coverage.sh
@@ -13,8 +13,8 @@ options=(--venv --venv-system-site-packages --color -v)
 
 ansible-test coverage combine --export "${agent_temp_directory}/coverage/" "${options[@]}"
 
-if ansible-test coverage analyze targets generate --help >/dev/null 2>&1; then
-    # Only analyze coverage if the installed version of ansible-test supports it.
-    # Doing so allows this script to work unmodified for multiple Ansible versions.
-    ansible-test coverage analyze targets generate "${agent_temp_directory}/coverage/coverage-analyze-targets.json" "${options[@]}"
+if ansible-test coverage analyze targets generate --help > /dev/null 2>&1; then
+  # Only analyze coverage if the installed version of ansible-test supports it.
+  # Doing so allows this script to work unmodified for multiple Ansible versions.
+  ansible-test coverage analyze targets generate "${agent_temp_directory}/coverage/coverage-analyze-targets.json" "${options[@]}"
 fi
--- .azure-pipelines/scripts/publish-codecov.sh
+++ .azure-pipelines/scripts/publish-codecov.sh
@@ -10,18 +10,18 @@ output_path="$1"
 curl --silent --show-error https://ansible-ci-files.s3.us-east-1.amazonaws.com/codecov/codecov.sh > codecov.sh
 
 for file in "${output_path}"/reports/coverage*.xml; do
-    name="${file}"
-    name="${name##*/}"  # remove path
-    name="${name##coverage=}"  # remove 'coverage=' prefix if present
-    name="${name%.xml}"  # remove '.xml' suffix
+  name="${file}"
+  name="${name##*/}" # remove path
+  name="${name##coverage=}" # remove 'coverage=' prefix if present
+  name="${name%.xml}" # remove '.xml' suffix
 
-    bash codecov.sh \
-        -f "${file}" \
-        -n "${name}" \
-        -X coveragepy \
-        -X gcov \
-        -X fix \
-        -X search \
-        -X xcode \
-        || echo "Failed to upload code coverage report to codecov.io: ${file}"
+  bash codecov.sh \
+    -f "${file}" \
+    -n "${name}" \
+    -X coveragepy \
+    -X gcov \
+    -X fix \
+    -X search \
+    -X xcode ||
+    echo "Failed to upload code coverage report to codecov.io: ${file}"
 done
