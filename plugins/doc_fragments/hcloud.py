62bde561f8a9d841f4d272c057695d31fd214d77
jo <ljonas@riseup.net>
2023-11-17 15:26:13 +0100

    style: format yaml files

--- plugins/doc_fragments/hcloud.py
+++ plugins/doc_fragments/hcloud.py
@@ -5,22 +5,22 @@
 class ModuleDocFragment:
     DOCUMENTATION = """
 options:
-    api_token:
-        description:
-            - This is the API Token for the Hetzner Cloud.
-            - You can also set this option by using the environment variable HCLOUD_TOKEN
-        required: True
-        type: str
-    endpoint:
-        description:
-            - This is the API Endpoint for the Hetzner Cloud.
-        default: https://api.hetzner.cloud/v1
-        type: str
+  api_token:
+    description:
+      - This is the API Token for the Hetzner Cloud.
+      - You can also set this option by using the environment variable HCLOUD_TOKEN
+    required: True
+    type: str
+  endpoint:
+    description:
+      - This is the API Endpoint for the Hetzner Cloud.
+    default: https://api.hetzner.cloud/v1
+    type: str
 requirements:
   - python-dateutil >= 2.7.5
   - requests >=2.20
 seealso:
-- name: Documentation for Hetzner Cloud API
-  description: Complete reference for the Hetzner Cloud API.
-  link: https://docs.hetzner.cloud/
+  - name: Documentation for Hetzner Cloud API
+    description: Complete reference for the Hetzner Cloud API.
+    link: https://docs.hetzner.cloud/
 """
