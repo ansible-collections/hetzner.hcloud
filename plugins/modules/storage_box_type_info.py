05de5bc3f713bf43e74abfe05ff56c3209b0943b
jo <ljonas@riseup.net>
2025-11-18 16:05:46 +0100

    copyright

--- plugins/modules/storage_box_type_info.py
+++ plugins/modules/storage_box_type_info.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright: (c) 2019, Hetzner Cloud GmbH <info@hetzner-cloud.de>
+# Copyright: (c) 2025, Hetzner Cloud GmbH <info@hetzner-cloud.de>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 
